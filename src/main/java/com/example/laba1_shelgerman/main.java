b8eb38e7f5c3465357aa2e74f0939b07a06056ce
Dr000per <germanwf03@gmail.com>
2026-06-14 16:27:30 +0300

    Проверка

--- src/main/java/com/example/laba1_shelgerman/main.java
+++ src/main/java/com/example/laba1_shelgerman/main.java
@@ -15,7 +15,7 @@ public class main extends Application {
 
         Scene scene = new Scene(loader.load(), 600, 300);
 
-        stage.setTitle("Конвертер рабочего времени");
+        stage.setTitle("Конверер рабочего времени");
         stage.setScene(scene);
         stage.show();
     }
